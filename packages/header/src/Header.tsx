2bea952011cacad5a6f0d8823e442f3294e17991
kahkeen-lai <kahkeen.lai@kloudius.com>
2024-04-24 14:45:57 +0800

    sixth commit

--- packages/header/src/Header.tsx
+++ packages/header/src/Header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export function Header() {
-  return <header>Lerna is the original monorepo tool! test3</header>;
+  return <header>Lerna is the original monorepo tool! test6</header>;
 }
